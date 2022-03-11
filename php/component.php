50db1dd10abd19e0e04fdd60001b513a488ad29e
Hamad Bakeel <hamad200714@gmail.com>
2022-03-12 01:22:57 +0300

    Feat: remove items from the cart

--- php/component.php
+++ php/component.php
@@ -35,4 +35,32 @@
         echo $element   ;
     }
 
+    function cartProuct($productImage, $productName, $productPrice, $productid){
+        $element = "
+        
+        <form action=\"cart.php?action=remove&id=$productid\" method=\"post\" class=\"cart-items\">
+                        <div class=\"border rounded p-3\">
+                            <div class=\"row bg-white\">
+                                <div class=\"col-md-3 \">
+                                    <img src=\"../uploads/$productImage\" alt=\"product image\">
+                                </div>
+                                <div class=\"col-md-6\">
+                                    <h5 class=\"pt-2\">$productName</h5>
+                                    <small class=\"text-secondary\">Seller: Samsung</small>
+                                    <h5 class=\"pt-2\">$$productPrice</h5>
+                                    <button type=\"submit\" class=\"btn btn-warning\">Save For Later</button>
+                                    <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Remove</button>
+                                </div>
+                                <div class=\"col-md-3 py-5\">
+                                    <button class=\"bg-light btn border rounded-circle\"><i class=\"fa-solid fa-minus\"></i></button>
+                                    <input type=\"text\" value=\"1\" class=\"form-control w-25 px-2 d-inline\">
+                                    <button class=\"bg-light btn border rounded-circle\"><i class=\"fa-solid fa-plus\"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+        ";
+        echo $element;
+    }
 ?>
+
